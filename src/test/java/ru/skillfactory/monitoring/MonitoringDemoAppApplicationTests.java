c1c3cb9c2e56a6c691311be71d204a65adb6a534
Nikolay Kostin <nikolay-2708@yandex.ru>
2025-11-18 19:38:19 +0300

    add project

---
+++ src/test/java/ru/skillfactory/monitoring/MonitoringDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillfactory.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
